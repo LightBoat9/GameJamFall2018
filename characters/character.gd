af184dd05f9642cc73eebbceedb1c14913423123
Luke <gamersloop@gmail.com>
2018-11-03 20:44:51 -0400

    Basics

--- characters/character.gd
+++ characters/character.gd
@@ -1,45 +1,156 @@
 extends KinematicBody2D
 
 enum {MODE_DEFEND, MODE_ATTACK}
+enum states {NORMAL, PECK, STUNNED}
+
+var state = states.NORMAL
 
 var mode = MODE_DEFEND setget set_mode
 
-var mode_speeds = [10, 15]
-var mode_animations = ['defend', 'attack']
+var mode_maxspeeds = [150, 200]
+var mode_animspeeds = [5, 8]
 
-var movespeed = mode_speeds[mode]
-var direction = 0
+var acceleration = 15
+var deceleration = 20
+
+var movespeed = 0.0
+var maxspeed = mode_maxspeeds[mode]
+export var direction = 1
 
 var velocity = Vector2()
-var gravity = 1
-var jumpspeed = 20
+var gravity = 15  # Subtracted from the vertical velocity every step
+var drop_speed = 20  # Extra gravity that the player can add
+var jumpspeed = 475
+var on_ground = false
 
 onready var asprite = $AnimatedSprite
+onready var attack_up = $AttackDown
+onready var attack_down = $AttackUp
+onready var up_offset = attack_up.position.x
+onready var down_offset = attack_down.position.x
+onready var asprite_offset = asprite.offset.x
+var can_hit_up = false
+var can_hit_down = false
+var enemy_up = null
+var enemy_down = null
+var knockback = Vector2(500, -350)
+var h_input = 0
+var stun_direction = 1
+
+func _ready():
+	attack_up.connect("body_entered", self, "body_event", ['up', true])
+	attack_up.connect("body_exited", self, "body_event", ['up', false])
+	attack_down.connect("body_entered", self, "body_event", ['down', true])
+	attack_down.connect("body_exited", self, "body_event", ['down', false])
+	asprite.connect("animation_finished", self, "anim_finished")
+
+func body_event(body, dir, entered):
+	if body != self and filename == body.filename:
+		if dir == 'up':
+			enemy_up = body if entered else null
+		if dir == 'down' and can_hit_down:
+			enemy_down = body if entered else null
+			
+func _input(event):
+	if state == states.NORMAL:
+		if event.is_action_pressed(Controllers.get_key(self, Controllers.MODE_SWAP)):
+			set_mode(1 - mode)
+		elif event.is_action_pressed(Controllers.get_key(self, Controllers.BASIC_ATTACK)) and on_ground:
+			state = states.PECK
+			velocity.x = 0
+			h_input = 0
+			asprite.animation = 'peck'
+			asprite.playing = true
 
 func _physics_process(delta):
-	if Input.is_action_just_pressed("mode_attack"):
-		set_mode(1 - mode)
+	on_ground = false
+	if state == states.NORMAL:
+		_movement_input()
+		_movement()
+		_animate()
+	elif state == states.STUNNED:
+		_movement()
+		if on_ground:
+			state = states.NORMAL
+		_animate()
+	elif state == states.PECK:
+		_movement()
+	
+func _movement_input():
+	h_input = (int(Input.is_action_pressed(Controllers.get_key(self, Controllers.MOVE_RIGHT))) 
+		- int(Input.is_action_pressed(Controllers.get_key(self, Controllers.MOVE_LEFT))))
 		
-	var direction = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	asprite.playing = h_input
 	
-	velocity.x = direction * movespeed
-	velocity.y = velocity.y + gravity
-	move_and_slide(velocity * movespeed)
+	if Input.is_action_pressed(Controllers.get_key(self, Controllers.DROP)):
+		velocity.y += drop_speed
+		
+	if Input.is_action_just_released(Controllers.get_key(self, Controllers.JUMP)):
+		if velocity.y < -jumpspeed / 2:
+			velocity.y = -jumpspeed / 2
+		
+func _movement():
+	if state != states.STUNNED:
+		if h_input:
+			direction = h_input
+			# Accelerate
+			if movespeed < maxspeed:
+				movespeed = min(maxspeed, movespeed + deceleration)
+		else:
+			# Decelerate
+			if movespeed > 0:
+				movespeed = max(0, movespeed - deceleration)
+		
+	velocity.x = movespeed * (h_input if state != states.STUNNED else stun_direction)
+	velocity.x = clamp(velocity.x, -maxspeed, maxspeed)
+	velocity.y += gravity
 	
-	# Update sprites flip when direction is not 0
-	if direction:
-		asprite.flip_h = direction == -1
+	move_and_slide(velocity)
 	
 	if get_slide_count():
 		var collision = get_slide_collision(get_slide_count() - 1)
 		
-		if collision and collision.normal == Vector2(0, -1):
-			velocity.y = 0
-			
-			if Input.is_action_just_pressed("jump"):
-				velocity.y = -jumpspeed
+		if collision:
+			if collision.normal == Vector2(0, -1):
+				velocity.y = 0
+				on_ground = true
 				
+				if Input.is_action_just_pressed(Controllers.get_key(self, Controllers.JUMP)):
+					velocity.y = -jumpspeed
+					
+			elif collision.normal == Vector2(0, 1):
+				velocity.y = 0
+	
+func _animate():
+	if velocity.x != 0:
+		if on_ground:
+			asprite.animation = 'walk'
+	else:
+		if on_ground:
+			asprite.animation = 'idle'
+			
+	if not on_ground:
+		if velocity.y > 0:
+			asprite.animation = 'down'
+		else:
+			asprite.animation = 'up'
+			
+	asprite.offset.x = asprite_offset * direction
+	asprite.flip_h = direction == -1
+	attack_up.position.x = up_offset * direction
+	attack_down.position.x = down_offset * direction
+					
 func set_mode(m):
 	mode = m
-	movespeed = mode_speeds[mode]
-	asprite.animation = mode_animations[mode]
+	maxspeed = mode_maxspeeds[mode]
+	asprite.frames.set_animation_speed('walk', mode_animspeeds[mode])
+	
+func anim_finished():
+	if asprite.animation == 'peck':
+		if enemy_up != null:
+			enemy_up.movespeed = knockback.x
+			enemy_up.stun_direction = direction
+			enemy_up.direction = -direction
+			enemy_up.velocity.y = knockback.y
+			enemy_up.state = states.STUNNED
+		state = states.NORMAL
---
+++ singletons/Controllers.gd
@@ -0,0 +1,14 @@
+extends Node
+
+enum {MOVE_RIGHT, MOVE_LEFT, DROP, JUMP, BASIC_ATTACK, MODE_SWAP}
+
+var keyboard_1 = ['k1_move_right', 'k1_move_left', 'k1_drop', 'k1_jump', 'k1_basic_attack', 'k1_mode_swap']
+var keyboard_2 = ['k2_move_right', 'k2_move_left', 'k2_drop', 'k2_jump', 'k2_basic_attack', 'k2_mode_swap']
+
+var binds = [keyboard_1, keyboard_2]
+var characters = []
+
+func get_key(character, index):
+	if not character in characters:
+		characters.append(character)
+	return binds[characters.find(character)][index]
